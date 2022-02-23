e4df79e0053f603a2ae64e6b7cac1b024b7ea9ba
Aleksey <aleksejbaskirov31@gmail.com>
2022-02-23 22:27:59 +0300

    DONE homework_lesson_15__21_02_2022_term_1_Square_figure.py

--- homework_lesson_15__21_02_2022_term_1_Square_figure.py
+++ homework_lesson_15__21_02_2022_term_1_Square_figure.py
@@ -1,35 +1,21 @@
 from math import pi, pow
 
 
-def square_figure(**kwargs):
-    temp = list()
-    # for key in kwargs:
-        # if key == 'rhombus':
-        #     for value in kwargs.values():
-        #         if type(value) == int:
-        #             temp.append(value)
-        #     return temp[0] * temp[1] / 2
-    if kwargs.keys() == 'square':
-        for value in kwargs.values():
-            print(value)
-            # if type(value) == int:
-            #     temp.append(value)
-        # return pow(temp[0], 2)
-        # elif key == 'trapezoid':
-        #     for value in kwargs.values():
-        #         if type(value) == int:
-        #             temp.append(value)
-        #     return ((temp[0] + temp[1]) * temp[3]) / 2
-        # elif key == 'circle':
-        #     for value in kwargs.values():
-        #         if type(value) == int:
-        #             return pi * pow(value, 2)
-        # else:
-        #     print("invalid data")
+def square_figure(figure_type, **kwargs):
+    if figure_type == 'rhombus':
+        return (kwargs['d1'] * kwargs['d2']) / 2
+    elif figure_type == 'square':
+        return pow(kwargs['a'], 2)
+    elif figure_type == 'trapezoid':
+        return ((kwargs['a'] + kwargs['b']) * kwargs['h']) / 2
+    elif figure_type == 'circle':
+        return pow(kwargs['r'], 2) * pi
+    elif figure_type == 'unknown':
+        return "invalid data"
 
 
-# print(square_figure(figure_type='rhombus', d1=10, d2=8))
+print(square_figure(figure_type='rhombus', d1=10, d2=8))
 print(square_figure(figure_type='square', a=5))
-# print(square_figure(figure_type='trapezoid', a=12, b=3, h=6))
-# print(square_figure(figure_type='circle', r=18))
-# print(square_figure(figure_type='unknown', a=1, b=2, c=3))
+print(square_figure(figure_type='trapezoid', a=12, b=3, h=6))
+print(square_figure(figure_type='circle', r=18))
+print(square_figure(figure_type='unknown', a=1, b=2, c=3))
