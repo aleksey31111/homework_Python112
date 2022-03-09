8269c3419c4277157928cbba6cd525b2fa8dff19
Aleksey <aleksejbaskirov31@gmail.com>
2022-03-09 22:25:56 +0300

    CONTINUE to Do homework_lesson_19__7_03_2022_3_Task_Find_Find_change_Find_world_begin_e.py

--- homework_lesson_19__7_03_2022_3_Task_Find_Find_change_Find_world_begin_e.py
+++ homework_lesson_19__7_03_2022_3_Task_Find_Find_change_Find_world_begin_e.py
@@ -12,3 +12,34 @@ print()
 print()
 
 ### Term 2 ###
+print("Задание 2: 1) Найти в СТРОКЕ Указанную ПОДСТРОКУ.\n"
+      "         2) Заменить ЕЕ на НОВУЮ.\n"
+      "СТРОКУ, ЕЕ ПОДСТРОКУ для Замены и НОВУЮ ПОДСТРОКУ Вводит Пользователь.\n")
+# string = input("Строка: ")
+# substring_for_change = input("Ее заменяемая подстрока: ")
+# new_substring = input("Новая подстрока: ")
+string = "11 23 44 55 23 22"
+substring_for_change = "23"
+new_substring = "!!!"
+new_substring_in_string = []
+for world in string:
+    if substring_for_change.find(world) != -1:
+        itr = iter(string)
+        second = next(itr)
+        if second == substring_for_change[1]:
+            new_substring_in_string = string[:substring_for_change.find(world)]+substring_for_change+[substring_for_change.find(world)+1]
+print(new_substring_in_string)
+#     start_index_substring_for_change = string.find(substring_for_change)
+#     start_index_substring_for_change_2 = string.find(substring_for_change, start_index_substring_for_change + 1)
+# index_of_substring = [ind for ind in range(len(string))]
+# print(index_of_substring)
+#     start_index_substring_for_change = string.find(world)
+#     if string[world] == substring_for_change[0]:
+#         print(world)
+# itr = iter(string)
+# print(next(itr))
+
+    # print(world)
+# print(world)
+# print(start_index_substring_for_change)
+# print(start_index_substring_for_change_2)
