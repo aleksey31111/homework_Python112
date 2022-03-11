10e5ea37a31cfb0ab689e9964473cd2e9d11ebbf
Aleksey <aleksejbaskirov31@gmail.com>
2022-03-12 02:03:17 +0300

    DONE homework_lesson_19__7_03_2022_3_Task_Find_Find_change_Find_world_begin_e.py

--- homework_lesson_19__7_03_2022_3_Task_Find_Find_change_Find_world_begin_e.py
+++ homework_lesson_19__7_03_2022_3_Task_Find_Find_change_Find_world_begin_e.py
@@ -21,10 +21,10 @@ def change_num(s, new, old):
     buffer_str_1 = ""
     char = 0
     while char < len(s):
-        if s[char] == old[0]:
+        if s[char] == old[0] and s[char+1]==old[1]:
             buffer_str_1 += new[0]
             buffer_str_1 += new[1]
-        elif s[char] == old[1]:
+        elif s[char] == old[1] and s[char-1] == old[0]:
             buffer_str_1 += new[2]
         else:
             buffer_str_1 += s[char]
@@ -35,9 +35,10 @@ def change_num(s, new, old):
 
 
 str1 = "11 23 44 55 23 22"
-str2 = change_num(str1, '!!!', '23')
-print("str1 = " + str1)
-print("str2 = " + str2)
+str2 = change_num(str1[::-1], '!!!', '32')
+print(f"str1 = {str1}")
+print(f"str2 = {str2[::-1]}")
+
 print()
 print()
 
@@ -49,51 +50,16 @@ piece_of_poetre = (
     f"Ежевику еле-еле"
     f"Ежата возле ели съели."
 )
-list_piece_of_poetre = list(piece_of_poetre)
 e = "е"
 E = "Е"
-print(e, end=";")
-print(E, end=";")
-ord_e = ord(e)
-ord_E = ord(E)
-print(ord("е"), end=";")
-print(ord("Е"), end=";")
-print(piece_of_poetre.count("Е"), end=";")
-print(piece_of_poetre.count("е"), end=";")
-print(piece_of_poetre.find(e), end=";")
-print(piece_of_poetre.find(E), end=";")
-print(len(piece_of_poetre))
-print(list(filter(lambda start_e: start_e.startswith(e), piece_of_poetre)))
-# for letter_e in piece_of_poetre:
-# number_e = 0
-# for char_e in piece_of_poetre:
-#     # number_e = 0
-# # if ord(piece_of_poetre[char_e]) == ord_e and piece_of_poetre[char_e] == ord_E and char_e - 1 == chr(
-# #         64) or char_e - 1 == chr(10):
-# #     number_e += 1
-# #     if ord(piece_of_poetre[char_e]) == ord_e and piece_of_poetre[char_e] == ord_E and char_e - 1 == chr(
-# #         64) or char_e - 1 == chr(10):
-#     if char_e == e and len(char_e) - 1 != :
-#         number_e += 1
-# print(number_e)
-# char_e = 0
-# while char_e < len(piece_of_poetre):
-#     if ord(piece_of_poetre[char_e]) == ord_e or ord(piece_of_poetre[char_e]) == ord(E)  and \
-#             piece_of_poetre[char_e - 1] != chr(0) and piece_of_poetre[char_e - 1] != chr(64) and \
-#             piece_of_poetre[char_e - 1] != chr(64):
-#         print(char_e, end=";")
-#     char_e += 1
+count_e = 0
+for char_e in range(len(piece_of_poetre)):
+    if piece_of_poetre[char_e] == e and ord(piece_of_poetre[char_e - 1]) == 32:
+        count_e += 1
 
-# char_e = 0
-# while char_e < len(piece_of_poetre):
-#     if ord(piece_of_poetre[char_e]) == ord_e:  # and \
-#             # piece_of_poetre[char_e - 1] != chr(0) and piece_of_poetre[char_e - 1] != chr(64) and \
-#             # piece_of_poetre[char_e - 1] != chr(64):
-#         print(piece_of_poetre.find(e))
-#     elif ord(piece_of_poetre[char_e]) == ord(E):
-#         print(piece_of_poetre.find(E))
-#         char_e += 1
-
-# if
-# print(piece_of_poetre.cou)
-# print(number_e)
+count_E = 0
+for char_E in range(len(piece_of_poetre)):
+    if piece_of_poetre[char_E] == E:
+        count_E += 1
+print()
+print(f"Количество слов: {count_e + count_E}")
