f3fc889dec6c56151e0cf08babc19b1047f61629
Aleksey <aleksejbaskirov31@gmail.com>
2022-03-25 21:07:06 +0300

    DONE homework_lesson_23__21_03_2022_2_term_decrement_incremenr_1_list_from_4_line_search_user_value.py -> homework_lesson_23__21_03_2022_1_Term_decrement_increment_1_list_from_4_line_search_user_value.py

--- homework_lesson_23__21_03_2022_1_Term_decrement_increment_1_list_from_4_line_search_user_value.py
+++ homework_lesson_23__21_03_2022_1_Term_decrement_increment_1_list_from_4_line_search_user_value.py
@@ -7,7 +7,7 @@ list2 = [3, 11, 8]
 list3 = [2, 4]
 list4 = [10, 1, 12]
 list5 = list1 + list2 + list3 + list4
-print(list5)
+
 
 
 def bubble_sorted_increment(lst):
@@ -17,19 +17,9 @@ def bubble_sorted_increment(lst):
                 lst[j], lst[j + 1] = lst[j + 1], lst[j]
 
 
-# bubble_sorted_increment(list5)
-# print(list5)
 
 
-def bubble_sorted_decrement(lst):
-    for i in range(len(lst) - 1):
-        for j in range(len(lst) - i -1):
-            if lst[j] < lst[j + 1]:
-                lst[j], lst[j + 1] = lst[j + 1], lst[j]
 
-
-# bubble_sorted_decrement(list5)
-# print(list5)
 def binary_search_increment(s, item):
     first = 0
     last = len(s) - 1
@@ -49,19 +39,22 @@ def binary_search_increment(s, item):
     else:
         print(f"Значене {search_in_bubble_sorted_increment} не найдено")
 
-def binary_search_decrement(s, item):
-    first = 0
-    last = len(s) - 1
+
+def bubble_sorted_decrement(lst):
+    for i in range(len(lst) - 1):
+        for j in range(len(lst) - i - 1):
+            if lst[j] < lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+
+
+def line_search_decrement(s, item):
+    pos = 0
     found = False
-    while first <= last and not found:
-        midpoint = (first + last) // 2
-        if s[midpoint] == item:
+    while pos < len(s) and not found:
+        if s[pos] == item:
             found = True
         else:
-            if item < s[midpoint]:
-                last = midpoint + 1
-            else:
-                first = midpoint - 1
+            pos = pos + 1
     # return found
     if found:
         print(f"Значене {search_in_bubble_sorted_decrement} найдено")
@@ -69,15 +62,16 @@ def binary_search_decrement(s, item):
         print(f"Значене {search_in_bubble_sorted_decrement} не найдено")
 
 
+# print(line_search_decrement(list5, 12))
 
-inp = int(input("1 - Сортировка по убыванию\n 2 - Сортировка по Возрастанию\n ->"))
+inp = int(input("1 - Сортировка по убыванию\n2 - Сортировка по Возрастанию\n ->"))
 if inp == 1:
     bubble_sorted_decrement(list5)
     print(list5)
     search_in_bubble_sorted_decrement = int(input("Введите значение для поиска: "))
-    binary_search_decrement(list5, search_in_bubble_sorted_decrement)
+    line_search_decrement(list5, search_in_bubble_sorted_decrement)
 elif inp == 2:
     bubble_sorted_increment(list5)
     print(list5)
     search_in_bubble_sorted_increment = int(input("Введите значение для поиска: "))
-
+    binary_search_increment(list5, search_in_bubble_sorted_increment)
