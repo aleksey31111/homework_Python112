477afe38bf5fdb9c42926c03b51090193b4de2e4
Aleksey <aleksejbaskirov31@gmail.com>
2022-04-23 15:34:13 +0300

    CONTINUE Do homework_lesson_30__13_04_2022_Abstract_class_root.py

--- homework_lesson_30__13_04_2022_Abstract_class_root.py
+++ homework_lesson_30__13_04_2022_Abstract_class_root.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from math import sqrt
+
 ### Task 1 ###
 print("Задание 1:\n"
       "\t1) Создать Базовый Абстракный Класс \"Корень\":\n"
@@ -11,11 +12,13 @@ print("Задание 1:\n"
 
 
 class Root(ABC):
-    def __init__(self, root1, root2=None, d=None):
-        if root2 is None or d is None:
-            self.root1 = root1
-        self.root1 = root1
-        self.root2 = root2
+    def __init__(self, a, b, c=None):
+        if c is None:
+            self.a = a
+            self.b = b
+        self.a = a
+        self.b = b
+        self.c = c
 
     @abstractmethod
     def root_quadratic_equation(self):
@@ -26,41 +29,49 @@ class Root(ABC):
         pass
 
     def print_root_linear_equation(self):
-        print(self.root1)
+        return -self.b / self.a
 
     def print_root_quadratic_equation(self):
-        print(self.root1, self.root2)
+        print((-self.b + sqrt(self.b ** 2 - 4 * self.a * self.c) / 2 * self.a),
+              (-self.b + sqrt(self.b ** 2 - 4 * self.a * self.c)) / 2 * self.a)
 
 
-class Linear(Root):
-    # def __init__(self, root1):
-    #     super(Linear, self).__init__(root1)
+class Linear(Root):  # 3x +7 = 0
+    def __init__(self, a, b):
+        super().__init__(self.a, self.b)
 
     def root_linear_equation(self):
-
-        self.root1 = -7 / 3
-        return self.root1
+        super().root_linear_equation()
+        return -self.b / self.a
 
     def print_root_linear_equation(self):
         super().print_root_linear_equation()
-        print(f"The root of '3x+7=0' is {self.root1}")
+        print(f"The root of '3x+7=0' is {-self.b / self.a}")
 
 
 class Quadratic(Root):  # x^2 - 2x - 3 = 0
-    # def __init__(self, d, root1, root2):
+    def __init__(self, a, b, c):
+        super().__init__(a, b, c)  # (-b + sqrt(d))/2a
+
     #     self.d = d  # b^2 - 4ac
     #     super(Quadratic, self).__init__(root1)  # (-b + sqrt(d))/2a
     #     self.root2 = root2  # (-b - sqrt(d))/2a
 
     def root_quadratic_equation(self):
-        self.d = pow(2, 2) - 4 * 1 * 3
-        self.root1 = (-2 + sqrt(self.d)) / 2 * 1
-        self.root2 = (-2 - sqrt(self.d)) / 2 * 1
-        return self.root1, self.root2
+        super().root_quadratic_equation()
+        d = pow(self.b, 2) - 4 * 1 * 3
+        root1 = (-self.b + sqrt(d)) / 2 * self.a
+        root2 = (-self.b - sqrt(d)) / 2 * self.a
+        return root1, root2
 
     def print_root_quadratic_equation(self):
-        print(f"The roots of '1x^2-2x-3=0' is: {self.root1}, {self.root2}")
+        super().print_root_quadratic_equation()
+
+        print(f"The roots of '1x^2-2x-3=0' is: {Quadratic.root_quadratic_equation()}")
 
 
-R = Root()
-# rle = Linear()
+r = Root
+l = Linear
+q = Quadratic
+# l(3, 7)
+# q(1, -2, -3)
