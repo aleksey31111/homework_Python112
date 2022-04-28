0e65364e2239e545bb9afe71d553b107bfdd87bc
Aleksey <aleksejbaskirov31@gmail.com>
2022-04-28 21:46:15 +0300

    DONE homework_lesson_30__13_04_2022_Abstract_class_root.py

--- homework_lesson_30__13_04_2022_Abstract_class_root.py
+++ homework_lesson_30__13_04_2022_Abstract_class_root.py
@@ -1,8 +1,6 @@
 from abc import ABC, abstractmethod
 from math import sqrt
 
-# from cmath import sqrt
-
 ### Task 1 ###
 print("Задание 1:\n"
       "\t1) Создать Базовый Абстракный Класс \"Корень\":\n"
@@ -24,49 +22,43 @@ class Root(ABC):
             self.b = b
             self.c = c
 
-    def calc_root_quadratic_equation(self):
-        raise NotImplementedError("В дочернем классе должен быть определен Метод print_root_linear_equation()")
+    @abstractmethod
+    def calc_root_equation(self):
+        pass
 
-    def calc_root_linear_equation(self):
-        raise NotImplementedError("В дочернем классе должен быть определен Метод print_root_linear_equation()")
+    @abstractmethod
+    def print_root_equation(self):
+        # raise NotImplementedError("В дочернем классе должен быть определен Метод print_root_linear_equation()")
+        pass
 
-    def print_root_linear_equation(self):
-        raise NotImplementedError("В дочернем классе должен быть определен Метод print_root_linear_equation()")
-
-    def print_root_quadratic_equation(self):
+        # @abstractmethod
+        # def print_root_quadratic_equation(self):
         raise NotImplementedError("В дочернем классе должен быть определен Метод print_root_quadratic_equation()")
-
+        # pass
+        pass
 
 class Linear(Root):  # 3x +7 = 0
-    def calc_root_linear_equation(self):
+    def calc_root_equation(self):
+        super().calc_root_equation()
         return -self.b / self.a
 
-    def print_root_linear_equation(self):
+    def print_root_equation(self):
         print(f"The root of '3x+7=0' is {round(-self.b / self.a, 2)}")
 
 
 class Quadratic(Root):  # x^2 - 2x - 3 = 0
-    def calc_root_quadratic_equation(self):
+    def calc_root_equation(self):
+        super().calc_root_equation()
         d = self.b ** 2 - 4 * self.a * self.c
         root1 = (-self.b + sqrt(abs(d))) / 2 * self.a
         root2 = (-self.b - sqrt(abs(d))) / 2 * self.a
         return round(root1, 3), round(root2, 3)
-        # (self.b + sqrt(self.b ** 2 - 4 * self.a * self.c)) / 2 * self.a, \
-        # (self.b - sqrt(self.b ** 2 - 4 * self.a * self.c)) / 2 * self.a
 
-    #
-    def print_root_quadratic_equation(self):
-        print(f"The roots of '1x^2-2x-3=0' is: {self.calc_root_quadratic_equation()} ")
-        # f"{(self.b + sqrt(self.b ** 2 - 4 * self.a * self.c)) / 2 * self.a}"
-        # f"{(self.b - sqrt(self.b ** 2 - 4 * self.a * self.c)) / 2 * self.a}")
+    def print_root_equation(self):
+        print(f"The roots of '1x^2-2x-3=0' is: {self.calc_root_equation()} ")
 
 
-# r = Root
 l = Linear(3, 7)
-l.print_root_linear_equation()
+l.print_root_equation()
 q = Quadratic(1, 2, 3)
-q.print_root_quadratic_equation()
-# print(l.__dict__)
-# q = Quadratic
-# l(3, 7)
-# q(1, -2, -3)
+q.print_root_equation()
