ef7466d61ddd3b5dcd2bb6a6d79686adb67eb87b
Aleksey <aleksejbaskirov31@gmail.com>
2022-05-06 22:29:05 +0300

    DONE homework_lesson_33__25.04.2022_Class_triangle_with_descriptor.py

--- homework_lesson_33__25.04.2022_Class_triangle_with_descriptor.py
+++ homework_lesson_33__25.04.2022_Class_triangle_with_descriptor.py
@@ -1,18 +1,19 @@
-class WholePositiveExistsTriangle:
-    @classmethod
-    def verify_positive_side(cls, side):
-        if side < 0:
-            raise ValueError(f"Сторона Треугольника {side} должна быть положительным числом")
+### task 1 ##
+print("Задание 1: Класс <Треугольник>, Дескриптор - Ввод сторон <int > 0>\n"
+      "\t\t   Проверка На существование Треугольника.")
+print()
 
+
+class WholePositiveExistsTriangle:
     @classmethod
     def verify_whole_number_of_side(cls, side):
         if not isinstance(side, int):
             raise TypeError(f"Сторона Треугольника {side} должна быть целым числом")
 
     @classmethod
-    def verify_exists_triangle(cls, ab, bc, ca):
-        if ab + bc > ca or bc + ca > ab or ca + ab > bc:
-            raise ValueError(f"Треугольника со сторонами {ab}, {bc}, {ca} Не Существкет.")
+    def verify_positive_side(cls, side):
+        if side < 0:
+            raise ValueError(f"Сторона Треугольника {side} должна быть положительным числом")
 
     def __set_name__(self, owner, name):
         self.name = name
@@ -22,8 +23,8 @@ class WholePositiveExistsTriangle:
         return instance.__dict__[self.name]
 
     def __set__(self, instance, value):
-        self.verify_positive_side(value)
         self.verify_whole_number_of_side(value)
+        self.verify_positive_side(value)
         # setattr(instance, self.name, value)
         instance.__dict__[self.name] = value
 
@@ -38,9 +39,22 @@ class Triangle:
         self.bc = bc
         self.ca = ca
 
+    def triangle_exists(self):
+        if self.ab + self.bc < self.ca or self.bc + self.ca < self.ab or self.ca + self.ab < self.bc:
+            return True
+        else:
+            return False
+
     def info(self):
-        return f"Треугольник Со Сторонами {[self.ab, self.bc, self.ca]} Сущесьвует."
+        if self.triangle_exists():
+            return f"Треугольника со сторонами {self.ab, self.bc, self.ca} не существкет."
+        else:
+            return f"Треугольник cо сторонами {self.ab, self.bc, self.ca} существует."
 
 
 t1 = Triangle(2, 5, 6)
 print(t1.info())
+t2 = Triangle(5, 2, 8)
+print(t2.info())
+t3 = Triangle(7, 3, 6)
+print(t3.info())
