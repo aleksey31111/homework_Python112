59d30500378b2fa5f46093b13dc3c868dc093f8e
Aleksey <aleksejbaskirov31@gmail.com>
2022-06-13 14:04:51 +0300

    Re DONE  homework_lesson_22__16_03_2022_2_Term_Count_element_in_nested_list_Validate_phone_namber.py

--- homework_lesson_22__16_03_2022_2_Term_Count_element_in_nested_list_Validate_phone_namber.py
+++ homework_lesson_22__16_03_2022_2_Term_Count_element_in_nested_list_Validate_phone_namber.py
@@ -1,52 +1,34 @@
 ### Term 1 ###
-print("Задание 1: Посчитать количество Элементов во ВЛОЖЕННОМ СПИСКЕ,\n \
+import re
+
+print("Задание 1: Валидация НОМЕРА ТЕЛЕФОНА.\n \
+ +7 499 456-45-78, +74994564578, 7 (499) 456 45 78, 7 (499) 456-45-78")
+phone_numer = "+7 499 456-45-78 , +74994564578, 7 (499) 456 45 78, 7 (499) 456-45-78"
+pat = r'\+7\s\d{3}\s\d{3}-\d{2}-\d{2}|[+7]\d{11}|[7]\s\(\d{3}\)\s\d{3}\s?-?\d{2}\s?-?\d{2}'
+print(re.findall(pat, phone_numer))
+
+
+### Term 2 ###
+print("Задание 2: Посчитать количество Элементов во ВЛОЖЕННОМ СПИСКЕ,\n \
       Не Используя РЕКУРСИЮ.")
-### Variant 1 ###
+
 names = ['Adam', ['Bob', ['Chet', 'Cat'], 'Bard', 'Bert'], 'Alex', ['Bea', 'Bill'], 'Ann']
 
 count = 0
-# print(len(names))
-for element in names:
-    if len(element[0]) < 2 and element[0].isupper():
-        print(element, end=" ")
-        count += 1
-    else:
-        for element1 in element:
-            if len(element1[0]) < 2 and element1[0].isupper():
-                print(element1, end=" ")
-                count += 1
-            else:
-                for element2 in element1:
-                    if len(element2[0]) < 2 and element2[0].isupper():
-                        print(element2, end=" ")
+for el_list in names:
+    if isinstance(el_list, list):
+        print(f"el_list: {el_list}")
+        for el_list1 in el_list:
+            if isinstance(el_list1, list):
+                print(f"el_list1: {el_list1}")
+                for el_list2 in el_list1:
+                    if isinstance(el_list2, list):
+                        print(f"el_list2: {el_list}")
+                    elif not isinstance(el_list2, list):
                         count += 1
-print()
-print(f"Количество Елементов в Списке: {count}")
-print()
-### Variant 2 ###
-print("Выпрямление СПИСКА с Использованием Рекурсии:")
+            elif not isinstance(el_list1, list):
+                count += 1
+    elif not isinstance(el_list, list):
+        count += 1
+print(count)
 
-
-def union(s):
-    if not s:  # s == []:
-        return s
-    if isinstance(s[0], list):
-        return union(s[0]) + union(s[1:])
-    return s[:1] + union(s[1:])
-
-
-names = ['Adam', ['Bob', ['Chet', 'Cat'], 'Bard', 'Bert'], 'Alex', ['Bea', 'Bill'], 'Ann']
-print("Выпрямленный СПИСОК: ", union(names))
-
-### Term 2 ###
-import re
-
-print("Задание 2: Валидация НОМЕРА ТЕЛЕФОНА.\n \
- +7 499 456-45-78, +74994564578, 7 (499) 456 45 78, 7 (499) 456-45-78")
-phone_numer = "+7 499 456-45-78 , +74994564578, 7 (499) 456 45 78, 7 (499) 456-45-78"
-# pat = r'[+]\s?[7]\s?([\d{3}]){2}\-([\d{2}]){2}'
-# pat = r"([+]\s?[\d]+\s[\d]+\s[\d]+\s[\d])|([+][\d]+)"  # True 2
-# pat = r"[+7]\s(:?[\d{3}])\s?(:?[\d{3}])-?(:?[\d{2}])-?(:?[\d]{2})  #|(:?[+][\d]+)"
-# pat = r'^\+7\s[\d{3}]\s[\d{3}]\-[\d{2}]\-[\d{2}]$'  #|[7]\s[(499)]\s[456]\s[45]\s[78]'
-pat = r'\+7\s\d{3}\s\d{3}-\d{2}-\d{2}|[+7]\d{11}|[7]\s\(\d{3}\)\s\d{3}\s?-?\d{2}\s?-?\d{2}'
-print(re.findall(pat, phone_numer))
