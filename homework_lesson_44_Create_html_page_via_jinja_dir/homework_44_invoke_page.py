348fd9297ccd38ccda872257034c7c7affc4ff34
Aleksey <aleksejbaskirov31@gmail.com>
2022-06-17 00:10:44 +0300

    DOWN  homework_lesson_44_Create_html_page_via_jinja_dir/homework_44_invoke_page.py

--- homework_lesson_44_Create_html_page_via_jinja_dir/homework_44_invoke_page.py
+++ homework_lesson_44_Create_html_page_via_jinja_dir/homework_44_invoke_page.py
@@ -1,13 +1,6 @@
 from jinja2 import Template, Environment, FileSystemLoader
 
 print("Homework 44 Invoke Page")
-# peaple = [
-#     {'name': "Евгений", 'born': 1995, 'height': 150 }
-#     {'name': "Евгения", 'born': 1996, 'height': 160 }
-#     {'name': "Анатолий", 'born': 1997, 'height': 170 }
-#     {'name': "Анастасия", 'born': 1998, 'height': 180 }
-#     {'name': "Прокопий", 'born': 1999, 'height': 190 }
-# ]
 
 homework = ["Страница с домашним заданием", "Домашнее задание выполненно"]
 
