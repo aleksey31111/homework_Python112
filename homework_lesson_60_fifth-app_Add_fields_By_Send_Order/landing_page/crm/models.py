5ea97e5eff54043647f891a87fbb6ad7a14322bf
Aleksey <aleksejbaskirov31@gmail.com>
2022-08-24 18:35:02 +0300

    DONE not correct Homework 60 add new_field in telebot and Order

---
+++ homework_lesson_60_fifth-app_Add_fields_By_Send_Order/landing_page/crm/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Статус заявки
+class StatusCrm(models.Model):
+    status_name = models.CharField(max_length=200, verbose_name="Название статуса")
+
+    def __str__(self):
+        return self.status_name
+
+    class Meta:
+        verbose_name = "Статус"
+        verbose_name_plural = "Статусы"
+
+
+# Заказы
+class Order(models.Model):
+    order_dt = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+    order_name = models.CharField(max_length=200, verbose_name="Имя")
+    order_phone = models.CharField(max_length=50, verbose_name="Телефон")
+    order_whats_happen = models.CharField(max_length=200, verbose_name="Причина неисправности")
+    order_service_name = models.CharField(max_length=200, verbose_name="Название услуги")
+    order_not_work = models.CharField(max_length=200, verbose_name="Что не работает")
+    order_status = models.ForeignKey(StatusCrm, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Статус')
+
+    def __str__(self):
+        return self.order_name
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+
+
+class CommentsCrm(models.Model):
+    comment_binding = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name="Заявка")
+    comment_text = models.TextField(verbose_name="Текст комментария")
+    comment_dt = models.DateTimeField(auto_now=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.comment_text
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
